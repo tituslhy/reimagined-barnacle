4f811250947db5726cfcce2f89fcacd8520a3b75
Titus Lim <tituslhy@gmail.com>
2025-04-21 11:14:18 +0800

    include mcp deployment

--- src/agent.py
+++ src/agent.py
@@ -1,46 +1,18 @@
-import httpx
 from dotenv import load_dotenv, find_dotenv
 
-from llama_index.core.tools import FunctionTool
 from llama_index.llms.openai import OpenAI
 from llama_index.core.agent.workflow import FunctionAgent, AgentStream
 from llama_index.core.workflow import Context
+from llama_index.tools.mcp import BasicMCPClient, McpToolSpec
 
 _ = load_dotenv(find_dotenv())
 
-def get_exchange_rate(
-    currency_from: str = "USD",
-    currency_to: str = "EUR",
-    currency_date: str = "latest",
-):
-    """Use this to get current exchange rate.
-
-    Args:
-        currency_from: The currency to convert from (e.g., "USD").
-        currency_to: The currency to convert to (e.g., "EUR").
-        currency_date: The date for the exchange rate or "latest". Defaults to "latest".
-
-    Returns:
-        A dictionary containing the exchange rate data, or an error message if the request fails.
-    """    
-    try:
-        response = httpx.get(
-            f"https://api.frankfurter.app/{currency_date}",
-            params={"from": currency_from, "to": currency_to},
-        )
-        response.raise_for_status()
-
-        data = response.json()
-        if "rates" not in data:
-            return {"error": "Invalid API response format."}
-        return data
-    except httpx.HTTPError as e:
-        return {"error": f"API request failed: {e}"}
-    except ValueError:
-        return {"error": "Invalid JSON response from API."}
-
-def get_agent():
-    tools = [FunctionTool.from_defaults(get_exchange_rate)]
+async def get_agent(mcp_client_url: str = "http://localhost:3000/sse"):
+    """Handler to create agents from MCP deployments"""
+    
+    mcp_client = BasicMCPClient(mcp_client_url)
+    mcp_tool = McpToolSpec(client=mcp_client)
+    tools = await mcp_tool.to_tool_list_async()
     agent = FunctionAgent(
         tools = tools,
         llm=OpenAI(model="gpt-4o-mini"),
--- src/task_manager.py
+++ src/task_manager.py
@@ -4,6 +4,9 @@ import logging
 import traceback
 from typing import AsyncIterable, Union, Dict, Any
 
+from llama_index.core.agent.workflow import FunctionAgent, AgentStream
+from llama_index.core.workflow import Context, StartEvent
+
 import os
 import sys
 
@@ -43,5 +46,86 @@ from common.types import (
 from common.server.task_manager import InMemoryTaskManager
 from common.utils.push_notification_auth import PushNotificationSenderAuth
 
-
+logger = logging.getLogger(__name__)
 # %%
+
+class CurrencyTaskManager(InMemoryTaskManager):
+    def __init__(
+        self, 
+        agent: FunctionAgent, 
+        notification_sender_auth: PushNotificationSenderAuth
+    ):
+        super().__init__()
+        self.agent = agent
+        self.notification_sender_auth = notification_sender_auth
+        self.ctx_states: Dict[str, Dict[str, Any]] = dict()
+    
+    def _get_input_event(self, task_send_params: TaskSendParams) -> StartEvent:
+        """Method to format request pings to a workflow start event"""
+        text_parts = []
+        for part in task_send_params.message_parts:
+            if isinstance(part, TextPart):
+                text_parts.append(part.text)
+            else:
+                raise ValueError(f"Unsupported part type: {type(part)}")
+        return StartEvent(
+            query = "\n".join(text_parts)
+        )
+    
+    async def _run_agent(self, request: SendTaskStreamingRequest):
+        """Main function that invokes the agent"""
+        
+        task_send_params: TaskSendParams = request.params
+        task_id = task_send_params.id
+        session_id = task_send_params.sessionId
+        start_event = self._get_input_event(task_send_params)
+        
+        try:
+            ctx, handler = None, None
+            
+            # Check if we have a saved context state for this session
+            print(f"Len of tasks: {len(self.tasks)}", flush=True)
+            print(f"Len of ctx_states: {len(self.ctx_states)}", flush=True)
+            saved_ctx_state = self.ctx_states.get(session_id, None)
+            
+            if saved_ctx_state is not None:
+                # Resume with existing context
+                logger.info(f"Resuming session {session_id} with saved context")
+                ctx = Context.from_dict(self.agent, saved_ctx_state)
+                handler = self.agent.run(start_event = start_event, ctx=ctx)
+            
+            else:
+                # Start a new session
+                logger.info(f"Starting new session {session_id}")
+                handler = self.agent.run(start_event=start_event)
+            
+            # Stream events as they come - for debugging
+            # async for event in handler.stream_events():
+            #     if isinstance(event, AgentStream):
+            #         print(event.delta, end="", flush=True)
+            
+            final_response = await handler
+            
+            ##### Postprocess final response #####
+            content = str(final_response)
+            parts = [
+                {
+                    "type": "text",
+                    "text": content
+                }
+            ]
+            self.ctx_states[session_id] = handler.ctx.to_dict()
+            artifact = Artifact(
+                parts=parts,
+                index=0,
+                append=False,
+            )
+            task_status = TaskStatus(state=TaskState.COMPELTED)
+            latest_task = await self.update_store(task_id, task_status, [artifact])
+            await self.send_task_notification(latest_task)
+            
+            # Send artifact updates
+            task_artifact
+            
+        except:
+            pass
